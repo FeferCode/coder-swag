b27718396c296e8735aed1e89795bcc8cf5b1434
Jakub Majewski <jakub.majewski@outlook.com>
2017-09-30 13:13:23 +0200

    This is the end of ep. 59

---
+++ coder-swag/coder-swag/View/CollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionCell.swift
+//  coder-swag
+//
+//  Created by Jakub Majewski on 30.09.2017.
+//  Copyright © 2017 Jakub Majewski. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    @IBOutlet weak var productImage:UIImageView!
+    @IBOutlet weak var productTitle:UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+}
